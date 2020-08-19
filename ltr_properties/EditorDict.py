70cfcaaf5f09ed38b91a834219aed62f19e148f5
Bill Clark <ltrandolphgames@gmail.com>
2020-08-19 16:23:49 -0700

    Support editing the keys of dictionaries, and adding/removing

--- ltr_properties/EditorDict.py
+++ ltr_properties/EditorDict.py
@@ -1,17 +1,48 @@
 from .CompoundEditor import CompoundEditor
+from .Icons import Icons
 
 from .TypeUtils import getDictKVTypeHints
 
 class EditorDict(CompoundEditor):
+    canDeleteElements = True
+
     def _getProperties(self):
         for name, value in self._targetObject.items():
-            setter = lambda val, thisName=name: self._setDictElem(thisName, val)
+            keySetter = lambda val, thisName=name: self._setDictKey(thisName, val)
+            valueSetter = lambda val, thisName=name: self._setDictValue(thisName, val)
 
             keyHint, valueHint = getDictKVTypeHints(self._typeHint)
 
-            yield name, value, setter, valueHint
+            yield name + " Key", name, keySetter, keyHint
+            yield name + " Value", value, valueSetter, valueHint
 
-    # This is a replacement for this, which isn't valid:
-    #  setter = lambda val, thisI=i: targetDict[thisName] = val
-    def _setDictElem(self, name, val):
+    def _setDictKey(self, name, val):
+        self._targetObject[val] = self._targetObject[name]
+        del self._targetObject[name]
+        self._createWidgetsForObject()
+
+    def _setDictValue(self, name, val):
         self._targetObject[name] = val
+
+    def _addClicked(self):
+        with self._editorGenerator.threadLock():
+            keyHint, valueHint = getDictKVTypeHints(self._typeHint)
+            keyInteger = 0
+            while str(keyInteger) in self._targetObject:
+                keyInteger += 1
+
+            self._targetObject[str(keyInteger)] = valueHint()
+
+            self._createWidgetsForObject()
+            self.dataChanged.emit(self._targetObject)
+
+    def _deleteClicked(self, name):
+        with self._editorGenerator.threadLock():
+            del self._targetObject[name]
+            self._createWidgetsForObject()
+            self.dataChanged.emit(self._targetObject)
+
+    def _getHeaderWidgets(self):
+        addButton = self._editorGenerator.createButton(Icons.Add)
+        addButton.clicked.connect(self._addClicked)
+        return [addButton]
