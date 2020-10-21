0ef7ac0433f0bea0c4ab7d6d57fd06697f14321b
Bill Clark <ltrandolphgames@gmail.com>
2020-10-21 14:39:23 -0700

    EditorDict supports removing keys/values on user editable

--- ltr_properties/EditorDict.py
+++ ltr_properties/EditorDict.py
@@ -9,12 +9,12 @@ class EditorDict(CompoundEditor):
     def _getProperties(self):
         keyHint, valueHint = getDictKVTypeHints(self._typeHint)
 
-        isUserEditableDict = self._typeHint and keyHint == str
+        self._isUserEditableDict = self._typeHint and keyHint == str
         
         for name, value in self._targetObject.items():
             keySetter = lambda val, thisName=name: self._setDictKey(thisName, val)
             valueSetter = lambda val, thisName=name: self._setDictValue(thisName, val)
-            if isUserEditableDict:
+            if self._isUserEditableDict:
                 yield name + " Key", name, keySetter, keyHint
                 yield name + " Value", value, valueSetter, valueHint
             else:
@@ -42,7 +42,10 @@ class EditorDict(CompoundEditor):
 
     def _deleteClicked(self, name):
         with self._editorGenerator.threadLock():
-            del self._targetObject[name]
+            if self._isUserEditableDict:
+                del self._targetObject[name.replace(" Key", "").replace(" Value", "")]
+            else:
+                del self._targetObject[name]
             self._createWidgetsForObject()
             self.dataChanged.emit(self._targetObject)
 
